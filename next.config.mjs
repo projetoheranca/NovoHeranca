027a675256bbc0d7dc512bf2f0babc85a63bb095
mayke <mayke@gmail.com>
2026-05-05 20:44:14 -0300

    fix: explicit CSP endpoints for Google Analytics

--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,13 @@ const nextConfig = {
   async headers() {
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.stripe.com https://js.stripe.com https://checkout.stripe.com https://www.google.com https://www.gstatic.com https://apis.google.com https://*.googleapis.com https://*.firebaseio.com https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com;
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.stripe.com https://js.stripe.com https://checkout.stripe.com https://www.google.com https://www.gstatic.com https://apis.google.com https://*.googleapis.com https://*.firebaseio.com https://www.googletagmanager.com https://*.googletagmanager.com https://www.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://analytics.google.com https://stats.g.doubleclick.net;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.googleapis.com https://*.stripe.com;
-      img-src 'self' blob: data: https://*.stripe.com https://placehold.co https://images.unsplash.com https://picsum.photos https://storage.googleapis.com https://firebasestorage.googleapis.com https://*.googleapis.com https://*.gstatic.com https://www.googletagmanager.com https://www.google-analytics.com;
+      img-src 'self' blob: data: https://*.stripe.com https://placehold.co https://images.unsplash.com https://picsum.photos https://storage.googleapis.com https://firebasestorage.googleapis.com https://*.googleapis.com https://*.gstatic.com https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://www.google.com.br https://stats.g.doubleclick.net;
       media-src 'self' blob: https://storage.googleapis.com https://firebasestorage.googleapis.com;
       font-src 'self' https://fonts.gstatic.com https://*.gstatic.com;
       frame-src 'self' https://*.stripe.com https://js.stripe.com https://hooks.stripe.com https://checkout.stripe.com https://accounts.google.com https://*.firebaseapp.com https://www.google.com https://*.google.com https://www.googletagmanager.com;
-      connect-src 'self' https://*.stripe.com https://api.stripe.com https://checkout.stripe.com https://*.googleapis.com wss://*.firebaseio.com https://*.firebase.com https://www.google-analytics.com https://region1.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
+      connect-src 'self' https://*.stripe.com https://api.stripe.com https://checkout.stripe.com https://*.googleapis.com wss://*.firebaseio.com https://*.firebase.com https://www.google-analytics.com https://region1.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://analytics.google.com https://www.google.com https://stats.g.doubleclick.net;
       form-action 'self' https://*.stripe.com https://checkout.stripe.com;
     `;
     return [
